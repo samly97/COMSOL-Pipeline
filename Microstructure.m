577489a11c41756d6b582acf8660e4d5a365b7d1
samly97 <97.sam.ly@gmail.com>
2021-09-11 20:54:26 -0400

    Added data formatting to Microstructure class.
    Values are only shown up to 3 digits of precision. Circle formatting function
    is crude since it is not vectorized, but this should not be bottlenecking

--- Microstructure.m
+++ Microstructure.m
@@ -9,9 +9,18 @@ classdef Microstructure
         function obj = Microstructure(id, ...
                 porosity, tortuosity, circles)
             obj.id = id;
-            obj.porosity = porosity;
-            obj.tortuosity = tortuosity;
-            obj.circles = circles;
+            obj.porosity = num2str(porosity, 3);
+            obj.tortuosity = num2str(tortuosity, 3);
+            obj.circles = Microstructure.Circles_to_Str(circles);
+        end
+    end
+    methods(Static)
+        function circles = Circles_to_Str(circles)
+            for c = 1:length(circles)
+                circles(c).x = num2str(circles(c).x * 10^6, 3);
+                circles(c).y = num2str(circles(c).y * 10^6, 3);
+                circles(c).R = num2str(circles(c).R * 10^6, 3);
+            end
         end
     end
 end
